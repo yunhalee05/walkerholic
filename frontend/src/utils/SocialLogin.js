22360c412767a22c05c0d46e5c8c448c1d19d23a
yunhalee05 <yunhalee0506@naver.com>
2022-05-27 15:56:58 +0900

    Refactor: ip 수정

--- frontend/src/utils/SocialLogin.js
+++ frontend/src/utils/SocialLogin.js
@@ -1,8 +1,8 @@
 
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://172.31.0.9:8080';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = 'http://172.31.0.9:3000/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorization/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorization/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
